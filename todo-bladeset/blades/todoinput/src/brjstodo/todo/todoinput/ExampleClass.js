4487e19a9761d493ada009e868b9ec075e498e23
Phil Leggetter <phil@leggetter.co.uk>
2013-10-01 13:19:33 +0100

    whitespace

--- todo-bladeset/blades/todoinput/src/brjstodo/todo/todoinput/ExampleClass.js
+++ todo-bladeset/blades/todoinput/src/brjstodo/todo/todoinput/ExampleClass.js
@@ -2,27 +2,27 @@ caplin.thirdparty( 'caplin-br' );
 
 ( function() {
 
-	var br = require( 'br' );
+  var br = require( 'br' );
 
-	var ServiceRegistry = require( 'br/ServiceRegistry' );
+  var ServiceRegistry = require( 'br/ServiceRegistry' );
 
-	function ExampleClass() {
-		this.message = new caplin.presenter.node.Field( "Hello World!" );
-		this.eventHub = ServiceRegistry.getService( 'event-hub' );
-	};
+  function ExampleClass() {
+    this.message = new caplin.presenter.node.Field( "Hello World!" );
+    this.eventHub = ServiceRegistry.getService( 'event-hub' );
+  };
 
-	br.extend( ExampleClass, caplin.presenter.PresentationModel );
+  br.extend( ExampleClass, caplin.presenter.PresentationModel );
 
-	ExampleClass.prototype.buttonClicked = function() {
-		var todoText = this.message.value.getValue();
+  ExampleClass.prototype.buttonClicked = function() {
+    var todoText = this.message.value.getValue();
     console.log( todoText );
 
     // TODO: validate
 
     this.eventHub.trigger( 'todo-added', { text: todoText } );
     this.message.value.setValue( '' );
-	}
+  }
 
-	brjstodo.todo.todoinput.ExampleClass = ExampleClass;
+  brjstodo.todo.todoinput.ExampleClass = ExampleClass;
 
 })();
