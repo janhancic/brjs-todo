187ff4e9714333e6a3d245aa0350c1847e8c41a8
Phil Leggetter <phil@leggetter.co.uk>
2013-10-09 12:51:00 +0100

    updating tests to use new DemoEventHub

--- todo-bladeset/blades/todoitems/tests/test-unit/js-test-driver/tests/ExampleClassTest.js
+++ todo-bladeset/blades/todoitems/tests/test-unit/js-test-driver/tests/ExampleClassTest.js
@@ -2,31 +2,55 @@ ExampleClassTest = TestCase("ExampleClassTest");
 
 caplin.thirdparty( 'caplin-br' );
 
-var stubEventHub = {
-	on: function( eventName, callback, context ) {
-		this.eventName = eventName;
-		this.callback = callback;
-		this.context = context;
-	}
-};
 
-var ServiceRegistry = require( 'br/ServiceRegistry' );
-ServiceRegistry.registerService( 'event-hub', stubEventHub );
+var fakeEventHub;
+var fakeChannel;
+
+ExampleClassTest.prototype.setUp = function() {
+
+  fakeChannel = {
+    on: function(eventName, callback, context) {
+      // store event name and data
+      this.eventName = eventName;
+      this.callback = callback;
+      this.context = context;
+    }
+  };
+
+  fakeEventHub = {
+    channel: function( channelName ) {
+      // store the name of the channel
+      this.channelName = channelName;
+      return fakeChannel;
+    }
+  };
+
+  var sr = require( 'br/ServiceRegistry' );
+
+  // ensure there isn't already an event-hub registered
+  sr.deregisterService( 'demo-event-hub' );
+
+  // Register the fake event hub
+  sr.registerService( 'demo-event-hub', fakeEventHub );
+};
 
 ExampleClassTest.prototype.testTodoItemsBladeListensToItemAddedEvents = function() {
 	var todoItemsBlade = new brjstodo.todo.todoitems.ExampleClass();
 
-	assertEquals( stubEventHub.eventName , 'todo-added' );
-	assertEquals( stubEventHub.context , todoItemsBlade );
+	assertEquals( fakeEventHub.channelName , 'todo-list' );
+	assertEquals( fakeChannel.eventName , 'todo-added' );
+	assertEquals( fakeChannel.context , todoItemsBlade );
 };
 
 ExampleClassTest.prototype.testItemsViewModelAddsItemOnTodoAddedEvent = function() {
+  var todoItemsBlade = new brjstodo.todo.todoitems.ExampleClass();
 
-	var todoItemsBlade = new brjstodo.todo.todoitems.ExampleClass();
+  var itemText = 'hello';
 
-	stubEventHub.callback.call( stubEventHub.context, { text: 'hello' } );
+  // trigger the callback
+  fakeChannel.callback.call( fakeChannel.context, { text: itemText } );
 
-	var items = todoItemsBlade.items.getPresentationNodesArray();
-	assertEquals( 'hello', items[ items.length - 1 ].value.getValue() );
-
+  // check the item has been added to the end of the list
+  var items = todoItemsBlade.items.getPresentationNodesArray();
+  assertEquals( itemText, items[ items.length - 1 ].value.getValue() );
 };
