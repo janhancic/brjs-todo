439bfd5880b5422b420e556e28d50f94e84e9389
Phil Leggetter <phil@leggetter.co.uk>
2013-10-10 17:24:38 +0100

    Moving back to a getting started, starting point

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-brjs-todo
-=========
+# BladeRunnerJS Getting Started Application Starting Point
+
+In future versions of BladeRunnerJS (BRJS) this starting point won't be required. Until then this represents a basic app outline that should be used when following the [BRJS Getting Started Guide](http://bladerunnerjs.org/docs/use/getting_started/).
--- default-aspect/src/brjstodo/App.js
+++ default-aspect/src/brjstodo/App.js
@@ -1,22 +1,6 @@
 ( function() {
 
   var App = function() {
-    var inputModel = new brjstodo.todo.todoinput.ExampleClass();
-    var itemsModel = new brjstodo.todo.todoitems.ExampleClass();
-
-    var PresenterComponent = br.presenter.component.PresenterComponent;
-    this.inputComponent = new PresenterComponent( 'brjstodo.todo.todoinput.view-template', inputModel );
-    this.itemsComponent = new PresenterComponent( 'brjstodo.todo.todoitems.view-template', itemsModel );
-    
-    this._appendComponent( this.inputComponent );
-    this._appendComponent( this.itemsComponent );
-  };
-
-  App.prototype._appendComponent = function( component ) {
-    component.setFrame(null);
-    var el = component.getElement();
-    document.getElementById( 'todoapp' ).appendChild( el );
-    component.onOpen();
   };
 
   brjstodo.App = App;
